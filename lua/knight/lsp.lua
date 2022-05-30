99e1ec977542333a5876dcd5454e33ab872dd285
Aleksei Abakumkin <cranberry.clockworks@gmail.com>
2022-05-30 19:52:27 +0300

    Update LUA language server config

--- lua/knight/lsp.lua
+++ lua/knight/lsp.lua
@@ -77,9 +77,32 @@ require('lspconfig').rust_analyzer.setup({
 -- Configure OmniSharp-Roslyn language server for C#
 -- https://github.com/neovim/nvim-lspconfig/blob/master/CONFIG.md#omnisharp
 local pid = vim.fn.getpid()
-local omnisharp_bin = "OmniSharp.exe"
+local omnisharp_bin = "omnisharp"
 
 require('lspconfig').omnisharp.setup({
     on_attach = on_attach,
     cmd = { omnisharp_bin, "--languageserver" , "--hostPID", tostring(pid) }
 })
+
+-- Lua
+require 'lspconfig'.sumneko_lua.setup {
+    settings = {
+        Lua = {
+            runtime = {
+                -- Tell the language server which version of Lua you're using (most likely LuaJIT in the case of Neovim)
+                version = 'LuaJIT',
+            },
+            diagnostics = {
+                -- Get the language server to recognize the `vim` global
+                globals = {'vim'},
+            },
+            workspace = {
+                -- Make the server aware of Neovim runtime files
+                library = vim.api.nvim_get_runtime_file("", true),
+            },
+            telemetry = {
+                enable = false,
+            },
+        }
+    }
+}
