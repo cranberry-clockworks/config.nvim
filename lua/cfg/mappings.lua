5ca0ae721748eb391ec4ef7669d7380c0c79c951
Aleksei Abakumkin <cranberry.knight@gmail.com>
2022-06-24 12:13:20 +0300

    Add mappings for diff

--- lua/cfg/mappings.lua
+++ lua/cfg/mappings.lua
@@ -20,6 +20,11 @@ function M.setup()
     vim.keymap.set('n', '<M-down>', "<cmd>lnext<cr>", { noremap = true })
     vim.keymap.set('n', '<M-up>', "<cmd>lprev<cr>", { noremap = true })
 
+    -- Diff
+    vim.keymap.set('n', '<leader>dw>', "<cmd>Gwrite<cr>", { noremap = true })
+    vim.keymap.set('n', '<leader>dl>', "<cmd>diffget //2 | diffupdate<cr>", { noremap = true })
+    vim.keymap.set('n', '<leader>dr>', "<cmd>diffget //3 | diffupdate<cr>", { noremap = true })
+
     -- Other
     vim.keymap.set('n', '<leader>ss', "<cmd>nohlsearch<cr>", { noremap = true })
 end
