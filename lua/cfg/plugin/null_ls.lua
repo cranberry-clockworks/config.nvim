68f6541b76db64524dafb229262a4267c3ea4eff
Aleksei Abakumkin <cranberry.knight@gmail.com>
2022-07-16 14:55:59 +0300

    Add stylua support to config

--- lua/cfg/plugin/null_ls.lua
+++ lua/cfg/plugin/null_ls.lua
@@ -1,9 +1,10 @@
-﻿local M = {}
+local M = {}
 
 function M.setup()
     local nls = require('null-ls')
 
-    local vale_config = vim.fn.expand(vim.opt.runtimepath:get()[1] .. '/vale.ini')
+    local vale_config =
+        vim.fn.expand(vim.opt.runtimepath:get()[1] .. '/vale.ini')
     nls.setup({
         sources = {
             nls.builtins.diagnostics.vale.with({
