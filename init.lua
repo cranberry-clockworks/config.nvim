b914ec7f420a61d4579e872619e41a884dab3f93
Aleksei Abakumkin <cranberry.clockworks@gmail.com>
2025-05-03 09:56:30 +0200

    Return the default setup handler for LSP

--- init.lua
+++ init.lua
@@ -424,6 +424,9 @@ require("lazy").setup({
 
             local lspconfig = require("lspconfig")
             require("mason-lspconfig").setup_handlers({
+                function(server_name)
+                    lspconfig[server_name].setup({})
+                end,
                 ["lua_ls"] = function()
                     lspconfig.lua_ls.setup({
                         settings = {
